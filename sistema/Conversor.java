0ee0e8e82443b008b8d330a430f268ba66794e2a
Daniel Lopes <dlopes4786@gmail.com>
2025-03-31 22:56:49 -0300

    Caso 1 e mudança na condição do while

--- sistema/Conversor.java
+++ sistema/Conversor.java
@@ -9,20 +9,19 @@ public class Conversor {
         System.out.println("1. Converter temperatura");
         System.out.println("2. Ver hisórico");
         System.out.println("4. Sair");
-        System.out.println("Escolha uma opção: ");
+        System.out.print("Escolha uma opção: ");
         int opcao_Escolhida = scanner.nextInt();
         System.out.println("");
 
-        float[] temperaturas = new float[5];
-        int posicao_Vetor = -1;
-        String unidade_Atual;
-        String unidade_Converter;        
+        double[] temperaturas = new double[5];
+        int posicao_Vetor = -1; 
+        double resultado = 0.0;      
         
-        while (true) {
+        while (opcao_Escolhida != 4) {
             switch (opcao_Escolhida) {
                 case 1:
                     System.out.println("[CONVERSÃO]");
-                    System.out.println("Digite a temperatura: ");
+                    System.out.print("Digite a temperatura: ");
                     if (posicao_Vetor == 5) {
                         posicao_Vetor = -1;
                     } //Caso chegue a 5 voltar á 1° posição
@@ -30,36 +29,46 @@ public class Conversor {
                         posicao_Vetor++;
                     } //limitar a apenas 5 posições
                     temperaturas[posicao_Vetor] = scanner.nextInt();
-                    System.out.println("Unidade atual (C/F/K): ");
-                    unidade_Atual = scanner.nextLine();
-                        switch (unidade_Atual) {
-                            case C:
+                    System.out.print("Unidade atual (C/F/K): ");
+                    String unidade_Atual = scanner.next().trim().toUpperCase(); //ler o valor e colocar em maiúsculo
+                    System.out.print("Converter para (C/F/K): ");
+                    String unidade_Converter = scanner.next().trim().toUpperCase();
 
-                                break;
-                            case F:
-                                
-                                break;
-                            case K:
-                                
-                                break;
-                            default:
-                                System.out.println("Opção inválida: ");
-                        }
-                    System.out.println("Converter para (C/F/K): ");
-                        switch (unidade_Converter) {
-                            case C:
-                                
-                                break;
-                            case F:
-                            
-                                break;
-                            case K:
-                            
-                                break;
-                            default:
-                                System.out.println("Opção inválida: ");
-                        }
-                    System.out.println("Resultado: ");
+                    //Conversão  
+                    if (unidade_Atual.equals("C")) { //Celsius 
+                        if (unidade_Converter.equals("F")) {
+                            resultado = (temperaturas[posicao_Vetor] * 9 / 5) + 32; //Celsius para Fahrenheit
+                        } else if (unidade_Converter.equals("K")) {
+                            resultado = temperaturas[posicao_Vetor] + 273.15; //Celsius para Kelvin
+                        } else {
+                            System.out.println("Opção inválida!");
+                            return;
+                        }    
+                    } else                   
+                    if (unidade_Atual.equals("F")) { //Fahrenheit
+                        if (unidade_Converter.equals("C")) {
+                            resultado = (temperaturas[posicao_Vetor] -32) * 5 / 9; //Fahrenheit para Celsius
+                        } else if (unidade_Converter.equals("K")) {
+                            resultado = (temperaturas[posicao_Vetor] -32) * 5 / 9 + 273.15; //Fahrenheit para Kelvin
+                        } else {
+                            System.out.println("Opção inválida!");
+                            return;
+                        }     
+                    } else
+                    if (unidade_Atual.equals("K")) { //Kelvin
+                        if (unidade_Converter.equals("C")) {
+                            resultado = temperaturas[posicao_Vetor] - 273.15; //Kelvin para Celsius
+                        } else if (unidade_Converter.equals("F")) {
+                            resultado = (temperaturas[posicao_Vetor] - 273.15) * 9 / 5 + 32; //Kelvin para Fahrenheit
+                        } else {
+                            System.out.println("Opção inválida!");
+                            return;
+                        }    
+                    } else {//Caso não seja nenhuma opção
+                        System.out.println("Opção inválida!");
+                        return;
+                    }
+                    System.out.printf("Resultado: %.2f %s\n", resultado , unidade_Converter);
                     System.out.println("");
                     break;
                 case 2:
@@ -76,10 +85,7 @@ public class Conversor {
                     System.out.println("Opção inválida!");
                     System.out.println("");
             }
-            if (opcao_Escolhida == 4){
-                System.out.println("Programa finalizado!");
-                break;
-            }
+
             System.out.println("== CONVERSOR DE TEMPERATURA ==");
             System.out.println("1. Converter temperatura");
             System.out.println("2. Ver hisórico");
@@ -93,6 +99,7 @@ public class Conversor {
 
 
         }
+        System.out.println("Programa finalizado!");
     }
     
 }
